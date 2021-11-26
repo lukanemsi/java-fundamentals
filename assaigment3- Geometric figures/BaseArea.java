7798f20dc07757e7c2c7ebc3f4ca6eb2e46336aa
lukanemsi <94932118+lukanemsi@users.noreply.github.com>
2021-11-26 21:59:52 +0400

    Create BaseArea.java

---
+++ assaigment3- Geometric figures/BaseArea.java
@@ -0,0 +1,11 @@
+package fop.w7geo;
+
+public abstract class BaseArea {
+    public abstract double circumference();
+
+    public abstract double area();
+
+    public abstract boolean isSquare();
+
+    public abstract Square toSquare();
+}
